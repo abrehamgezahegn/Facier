681ab7eee1fc9823397369615654332e93e75ba9
abrehamgezahegn <gezahegnabereham@gmail.com>
2021-08-12 16:32:47 +0300

    chore: removed api

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import AppRouter from "./Routers/AppRouter";
-import "font-awesome/css/font-awesome.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "font-awesome/css/font-awesome.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "tachyons";
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"));
